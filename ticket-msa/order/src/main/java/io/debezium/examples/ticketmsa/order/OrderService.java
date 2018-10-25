bbba626cbefa0859543fd8d9bfdeb5923e02e6f3
Gunnar Morling <gunnar.morling@googlemail.com>
2018-10-25 08:51:26 +0200

    Adding instructions for module 2

--- ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
+++ ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
@@ -1,14 +1,13 @@
 package io.debezium.examples.ticketmsa.order;
 
-import java.math.BigDecimal;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -19,7 +18,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.debezium.examples.ticketmsa.order.model.Order;
 
-@Path("/")
+@Path("/orders")
 @ApplicationScoped
 @KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
 public class OrderService {
@@ -34,11 +33,11 @@ public class OrderService {
     @Producer
     private SimpleKafkaProducer<Integer, JsonObject> kafka;
 
-    @GET
+    @POST
     @Transactional
     @Produces("application/json")
-    public Order addOrder() {
-        Order order = new Order("John", "Doe", "john.doe@example.com", new BigDecimal(1000));
+    @Consumes("application/json")
+    public Order addOrder(Order order) {
         order = entityManager.merge(order);
         kafka.send(topicName, order.getId(), order.toJson());
         return order;
