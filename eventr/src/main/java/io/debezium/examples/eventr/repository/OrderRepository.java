1374e709a0175d74c648fd0d6cd960a109b77c70
Gunnar Morling <gunnar.morling@googlemail.com>
2018-10-26 19:52:42 +0200

    #60 Triggering load of referenced Event entity for view materialization

--- eventr/src/main/java/io/debezium/examples/eventr/repository/OrderRepository.java
+++ eventr/src/main/java/io/debezium/examples/eventr/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ejb.Stateful;
 
+import io.debezium.examples.eventr.model.Event;
 import io.debezium.examples.eventr.model.Order;
 
 @Stateful
@@ -11,6 +12,13 @@ public class OrderRepository extends Repository<Order> {
 
     public OrderRepository() {
         super(Order.class);
+    }
+
+    @Override
+    public Order create(Order entity) {
+        entity.setEvent(entityManager.find(Event.class, entity.getEvent().getId()));
+        entityManager.persist(entity);
+        return entity;
     }
 
     public List<Order> getByEventId(int eventId) {
@@ -22,5 +30,11 @@ public class OrderRepository extends Repository<Order> {
                 .createQuery( "FROM Order o WHERE o.customer LIKE :term OR o.event.name LIKE :term", Order.class )
                 .setParameter( "term", "%" + term + "%" )
                 .getResultList();
+    }
+
+    @Override
+    public Order save(Order entity) {
+        entity.setEvent(entityManager.find(Event.class, entity.getEvent().getId()));
+        return entityManager.merge(entity);
     }
 }
