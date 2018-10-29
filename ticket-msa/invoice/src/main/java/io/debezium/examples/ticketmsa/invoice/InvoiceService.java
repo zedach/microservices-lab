5859a5d63e7bfe303016d8c0a44dbf01a2873e12
Jiri Pechanec <jpechane@redhat.com>
2018-10-29 09:48:38 +0100

    Implementing Kafka consumer

--- ticket-msa/invoice/src/main/java/io/debezium/examples/ticketmsa/invoice/InvoiceService.java
+++ ticket-msa/invoice/src/main/java/io/debezium/examples/ticketmsa/invoice/InvoiceService.java
@@ -2,11 +2,19 @@ package io.debezium.examples.ticketmsa.invoice;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.aerogear.kafka.cdi.annotation.Consumer;
+import org.aerogear.kafka.cdi.annotation.KafkaConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
+@KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
 public class InvoiceService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InvoiceService.class);
+
+    @Consumer(topics = "#{ORDER_TOPIC_NAME}", groupId = "InvoiceService")
+    public void orderArrived(final String order) {
+        LOGGER.info("Order event '{}' arrived", order);
+    }
 }
