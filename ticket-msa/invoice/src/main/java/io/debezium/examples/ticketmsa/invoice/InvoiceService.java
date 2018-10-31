1158d9608ff58bfbf8bb40ff82c1e2fce37ffcfd
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-10-31 16:11:51 +0100

    inject the required Kafka configuration

--- ticket-msa/invoice/src/main/java/io/debezium/examples/ticketmsa/invoice/InvoiceService.java
+++ ticket-msa/invoice/src/main/java/io/debezium/examples/ticketmsa/invoice/InvoiceService.java
@@ -1,11 +1,13 @@
 package io.debezium.examples.ticketmsa.invoice;
 
-import javax.enterprise.context.ApplicationScoped;
-
+import org.aerogear.kafka.cdi.annotation.KafkaConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.context.ApplicationScoped;
+
 @ApplicationScoped
+@KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
 public class InvoiceService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InvoiceService.class);
